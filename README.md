9a4a48b94bf7eab65d034b5e5d6d0e6b1fb0e137
Dan Allison <dan@danallison.org>
2013-01-08 07:07:45 -0600

    added js file and updated README

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 animate-text-content.js
 =======================
 
-JS micro-library for changing the text content of DOM elements over time
+This JS micro-library allows you to animate the text content of elements in the DOM. For example, if you have `<div id='number'>298</div>` and you want 298 to count up to 426 over 2 seconds, you write `animateTextContent.betweenTwoValues('number', 298, 426, 2);`.
+
+Caution
+=======
+Current build state is incomplete. Use at your own risk.
---
+++ animate-text-content.js
@@ -0,0 +1,26 @@
+var animateTextContent = {};
+
+animateTextContent.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                               'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+// animate numerical text between 2 Values, as in animating from, say, 40 to 95 over half a second.
+animateTextContent.betweenTwoValues = function(elementID, startValue, endValue, time){
+  var difference = endValue - startValue,
+      timeIncrement = time/difference,
+      element = document.getElementById(elementID);
+  
+  if(difference < 0){
+    difference = difference * -1;
+  }
+  
+  for (i = 1; i <= dif; i++) {
+    window.setTimeout(function() {
+      if (startValue < endValue) {
+        startValue = startValue + 1;
+      }else{
+        startValue = startValue - 1;
+      }
+      element.textContent = v1;
+    }, timeIncrement * i);
+  }
+};
