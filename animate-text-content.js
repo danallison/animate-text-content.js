db7e2e0dfd085a6146fdf9ede293d172910e0824
Dan Allison <dan@danallison.org>
2013-01-08 15:51:22 -0600

    added new functions

--- animate-text-content.js
+++ animate-text-content.js
@@ -12,14 +12,14 @@ animateTextContent.betweenTwoValues = function (elementID, startValue, endValue,
       nextFrame,
       i;
   
-  if(difference < 0){
+  if (difference < 0){
     difference = difference * -1;
     addFrame = -1;
   }
   
   frameRate = duration / difference;
   
-  nextFrame = function() {
+  nextFrame = function () {
     startValue = startValue + addFrame;
     element.textContent = startValue;
   };
@@ -27,4 +27,53 @@ animateTextContent.betweenTwoValues = function (elementID, startValue, endValue,
   for (i = 1; i <= difference; i++) {
     setTimeout(nextFrame, frameRate * i);
   }
+};
+
+animateTextContent.erase = function (elementID, duration) {
+  var element = document.getElementById(elementID),
+      originalText = element.textContent,
+      textArray = originalText.split(''),
+      len = originalText.length,
+      frameRate = duration / len,
+      nextFrame,
+      i;
+      
+  nextFrame = function () {
+    textArray.pop();
+    element.textContent = textArray.join('');
+  };
+  
+  for (i = 1; i <= len; i++) {
+    setTimeout(nextFrame, frameRate * i);
+  }
+};
+
+animateTextContent.typing = function (elementID, text, duration) {
+  var element = document.getElementById(elementID),
+      textArray = text.split(''),
+      typingArray = [],
+      len = text.length,
+      frameRate = duration / len,
+      nextFrame,
+      i;
+      
+  nextFrame = function () {
+    typingArray.push(textArray.shift());
+    element.textContent = typingArray.join('');
+  };
+  
+  for (i = 1; i <= len; i++) {
+    setTimeout(nextFrame, frameRate * i);
+  }
+};
+
+// TODO refactor this function to keep a consistent frame rate, get rid of `halfDuration`
+animateTextContent.eraseAndReplace = function (elementID, replacementText, duration) {
+  var halfDuration = duration / 2,
+      afterPause = function () {
+        animateTextContent.typing(elementID, replacementText, halfDuration)
+      };
+      
+  animateTextContent.erase( elementID, halfDuration );
+  setTimeout(afterPause, halfDuration);
 };
