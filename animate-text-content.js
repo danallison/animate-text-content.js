1739c34c1994aad3c60c602a4632f359a14da7c2
Dan Allison <dan@danallison.org>
2013-01-08 10:49:48 -0600

    refactored function betweenTwoValues for efficiency

--- animate-text-content.js
+++ animate-text-content.js
@@ -1,27 +1,30 @@
 var animateTextContent = {};
 
-animateTextContent.alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
-                               'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+animateTextContent.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+                               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // animate numerical text between 2 Values, as in animating from, say, 40 to 95 over half a second.
-animateTextContent.betweenTwoValues = function(elementID, startValue, endValue, duration){
+animateTextContent.betweenTwoValues = function (elementID, startValue, endValue, duration) {
   var difference = endValue - startValue,
-      element = document.getElementById(elementID);
+      element = document.getElementById(elementID),
+      addFrame = 1,
+      frameRate,
+      nextFrame,
+      i;
   
   if(difference < 0){
     difference = difference * -1;
+    addFrame = -1;
   }
   
-  var timeIncrement = duration/difference;
+  frameRate = duration / difference;
   
-  for (var i = 1; i <= difference; i++) {
-    window.setTimeout(function() {
-      if (startValue < endValue) {
-        startValue = startValue + 1;
-      }else{
-        startValue = startValue - 1;
-      }
-      element.textContent = startValue;
-    }, timeIncrement * i);
+  nextFrame = function() {
+    startValue = startValue + addFrame;
+    element.textContent = startValue;
+  };
+  
+  for (i = 1; i <= difference; i++) {
+    setTimeout(nextFrame, frameRate * i);
   }
 };
