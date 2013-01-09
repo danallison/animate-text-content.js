399cbef4e2a63d37400382bfdd61215a7815684e
Dan Allison <dan@danallison.org>
2013-01-09 07:48:22 -0600

    adding timeline functionality. still buggy.

--- animate-text-content.js
+++ animate-text-content.js
@@ -1,14 +1,23 @@
 var animateTextContent = function (elementID) {
-  var Animator = function (elementID) {
+  var Timeline = function (elementID) {
     this.element = document.getElementById(elementID);
+    this.frameRate = 1000/60;
+    this.queueDelay = 0;
   };
   
-  Animator.prototype.betweenTwoValues = function (startValue, endValue, duration) {
+  Timeline.prototype.addToQueue = function (funktion, duration) {
+    var thiz = this;
+    setTimeout(funktion, thiz.queueDelay);
+    thiz.queueDelay += duration;
+  };
+  
+  Timeline.prototype.betweenTwoValues = function (startValue, endValue, duration) {
     var thiz = this,
         difference = endValue - startValue,
         addFrame = 1,
         frameRate,
         nextFrame,
+        funktion,
         i;
 
     if (difference < 0){
@@ -16,75 +25,91 @@ var animateTextContent = function (elementID) {
       addFrame = -1;
     }
 
-    frameRate = duration / difference;
+    frameRate = duration / difference || thiz.frameRate;
 
     nextFrame = function () {
       startValue = startValue + addFrame;
       thiz.element.textContent = startValue;
     };
-
-    for (i = 1; i <= difference; i++) {
-      setTimeout(nextFrame, frameRate * i);
-    }
+    
+    funktion = function () {
+      for (i = 1; i <= difference; i++) {
+        setTimeout(nextFrame, frameRate * i);
+      }
+    };
+    
+    duration = duration || thiz.frameRate * difference;
+    thiz.addToQueue(funktion, duration);
     
     return thiz;
   };
   
-  Animator.prototype.erase = function (duration) {
+  Timeline.prototype.erase = function (duration) {
     var thiz = this,
         originalText = thiz.element.textContent,
         textArray = originalText.split(''),
         len = originalText.length,
-        frameRate = duration / len,
+        frameRate = duration / len || thiz.frameRate,
         nextFrame,
+        funktion,
         i;
 
     nextFrame = function () {
       textArray.pop();
       thiz.element.textContent = textArray.join('');
     };
-
-    for (i = 1; i <= len; i++) {
-      setTimeout(nextFrame, frameRate * i);
-    }
+    
+    funktion = function () {
+      for (i = 1; i <= len; i++) {
+        setTimeout(nextFrame, frameRate * i);
+      }
+    };
+    
+    duration = duration || thiz.frameRate * len;
+    thiz.addToQueue(funktion, duration);
     
     return thiz;
   };
 
-  Animator.prototype.typing = function (text, duration) {
+  Timeline.prototype.typing = function (text, duration) {
     var thiz = this,
         textArray = text.split(''),
         typingArray = [],
         len = text.length,
-        frameRate = duration / len,
+        frameRate = duration / len || thiz.frameRate,
         nextFrame,
+        funktion,
         i;
-
+    
     nextFrame = function () {
       typingArray.push(textArray.shift());
       thiz.element.textContent = typingArray.join('');
     };
-
-    for (i = 1; i <= len; i++) {
-      setTimeout(nextFrame, frameRate * i);
-    }
     
-    return thiz;
-  };
-
-  // TODO refactor this function to keep a consistent frame rate, get rid of `halfDuration`
-  Animator.prototype.eraseAndReplace = function (replacementText, duration) {
-    var thiz = this,
-        halfDuration = duration / 2,
-        afterPause = function () {
-          thiz.typing(replacementText, halfDuration);
-        };
-
-    thiz.erase(halfDuration);
-    setTimeout(afterPause, halfDuration);
+    funktion = function () {
+      for (i = 1; i <= len; i++) {
+        setTimeout(nextFrame, frameRate * i);
+      }
+    };
+    
+    duration = duration || len;
+    thiz.addToQueue(funktion, duration);
     
     return thiz;
   };
   
-  return new Animator(elementID);
+  Timeline.prototype.pause = function (duration) {
+    var thiz = this,
+    funktion = function () {};
+    
+    thiz.addToQueue(funktion, duration);
+    
+    return thiz;
+  };
+  
+  Timeline.prototype.clearQueue = function () {
+    this.queueDelay = 0;
+  };
+  
+  return new Timeline(elementID);
 };
